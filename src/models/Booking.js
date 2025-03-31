888fd8c10d9df1290f87d5c6c54cbac1f8086fef
Ved Padaria <135796391+vedpadaria@users.noreply.github.com>
2025-03-31 22:09:10 +0530

    Delete src directory

--- src/models/Booking.js
+++
@@ -1,5 +0,0 @@
-const bookingSchema = new mongoose.Schema({
-    trainId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Train' },
-    passengerDetails: [{ /* Your passenger details schema */ }],
-    // ... other fields
-});
